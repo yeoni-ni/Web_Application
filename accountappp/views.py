8b247c0d786289389507202ed0db8af85dd845e2
yeon-ni <kimsy0831@lilkelion.org>
2021-07-08 12:35:02 +0900

    7/8 6class commit 3

--- accountappp/views.py
+++ accountappp/views.py
@@ -5,4 +5,10 @@ from django.shortcuts import render
 
 def hello_world(request):
     # return HttpResponse('Hello World!') #단순 문자열 출력
-    return render(request, 'accountapp/hello_world.html') # 함수인자, 만든 html 불러오기
+    # 메서드 분류 해서 출력 위해 if 사용 (get, post)
+    if request.method == "POST":
+        return render(request, 'accountapp/hello_world.html',
+                      context={'text': 'POST METHOD'}) # 함수인자, 만든 html 불러오기
+    else:
+        return render(request, 'accountapp/hello_world.html',
+                      context={'text': 'GET METHOD'})
