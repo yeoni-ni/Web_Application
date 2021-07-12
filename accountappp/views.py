f450d5483fb1f39cfec3953e691aa23ce76d65d5
yeon-ni <kimsy0831@lilkelion.org>
2021-07-12 11:10:37 +0900

    7/12 6class commit1

--- accountappp/views.py
+++ accountappp/views.py
@@ -7,8 +7,10 @@ def hello_world(request):
     # return HttpResponse('Hello World!') #단순 문자열 출력
     # 메서드 분류 해서 출력 위해 if 사용 (get, post)
     if request.method == "POST":
+        temp = request.POST.get('input_text')
+
         return render(request, 'accountapp/hello_world.html',
-                      context={'text': 'POST METHOD'}) # 함수인자, 만든 html 불러오기
+                      context={'text': 'temp'})
     else:
         return render(request, 'accountapp/hello_world.html',
                       context={'text': 'GET METHOD'})
