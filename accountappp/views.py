61dbb2d32aa7dfb10dcad043bec7aaf438985ac7
yeon-ni <kimsy0831@lilkelion.org>
2021-07-12 11:37:58 +0900

    7/12 6class commit 2 sqlite_connect

--- accountappp/views.py
+++ accountappp/views.py
@@ -2,6 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from accountappp.models import HelloWorld
+
 
 def hello_world(request):
     # return HttpResponse('Hello World!') #단순 문자열 출력
@@ -9,8 +11,12 @@ def hello_world(request):
     if request.method == "POST":
         temp = request.POST.get('input_text')
 
+        new_hello_world=HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save()
+
         return render(request, 'accountapp/hello_world.html',
-                      context={'text': temp})
+                      context={'new_hello_world': new_hello_world}) # 객체반환
     else:
         return render(request, 'accountapp/hello_world.html',
                       context={'text': 'GET METHOD'})
