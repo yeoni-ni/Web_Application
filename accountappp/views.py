cdd65068479cb0816011ac391d5e3674f8fc7ed2
yeon-ni <kimsy0831@lilkelion.org>
2021-07-12 12:05:49 +0900

    7/12 6class commit 3

--- accountappp/views.py
+++ accountappp/views.py
@@ -9,14 +9,20 @@ def hello_world(request):
     # return HttpResponse('Hello World!') #단순 문자열 출력
     # 메서드 분류 해서 출력 위해 if 사용 (get, post)
     if request.method == "POST":
+
         temp = request.POST.get('input_text')
 
         new_hello_world=HelloWorld()
         new_hello_world.text = temp
         new_hello_world.save()
 
+        # DB읽어오기(클래스 안 모든 오브젝트 불러오기)
+        hello_world_list = HelloWorld.objects.all()
+
+
         return render(request, 'accountapp/hello_world.html',
-                      context={'new_hello_world': new_hello_world}) # 객체반환
+                      context={' hello_world_list': hello_world_list }) # 객체반환
     else:
+        hello_world_list = HelloWorld.objects.all()
         return render(request, 'accountapp/hello_world.html',
-                      context={'text': 'GET METHOD'})
+                      context={'hello_world_list': hello_world_list})
