067f1155f8d41171684b87e15d798e85c6ff93fa
yeon-ni <kimsy0831@lilkelion.org>
2021-07-19 12:26:43 +0900

    7/19 6class commit 2

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from accountapp.views import hello_world, AccountCreateView, AccountDetailView
+from accountapp.views import hello_world, AccountCreateView, AccountDetailView, AccountUpdateView, AccountDeleteView
 
 app_name ='accountapp'
 
@@ -13,8 +13,10 @@ urlpatterns = [
     path('logout/',LogoutView.as_view(),name='logout'),
     # 회원가입
     path('create/',AccountCreateView.as_view(),name='create'),
-    # Detail
+    # Detail 회원정보
     path('detail/<int:pk>', AccountDetailView.as_view(), name='detail'),
-    # Update
-    path('update/<int:pk>', AccountCreateView.as_view(), name='update')
+    # Update 회원정보변경
+    path('update/<int:pk>', AccountUpdateView.as_view(), name='update'),
+    # Delete 회원탈퇴
+    path('delete/<int:pk>', AccountDeleteView.as_view(), name='delete')
 ]
