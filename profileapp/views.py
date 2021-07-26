f4532aeecefefbe7b1d38142ab95d2a60dd34fc2
yeon-ni <kimsy0831@lilkelion.org>
2021-07-26 12:20:48 +0900

    7/26 6class commit3

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from profileapp.forms import ProfileCreationForm
+from profileapp.models import Profile
+
+
+class ProfileCreateView(CreateView):
+    model = Profile
+    # 입력 받는 form
+    form_class = ProfileCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'profileapp/create.html'
+
+
+
+
