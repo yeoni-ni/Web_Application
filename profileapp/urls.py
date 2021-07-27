c04927bce578aea71fb40b7b96bccf466bc249f7
kimsy0831 <kimsy0831@liklion.org>
2021-07-27 18:03:59 +0900

    7/27 6class commit4

--- profileapp/urls.py
+++ profileapp/urls.py
@@ -4,5 +4,6 @@ from profileapp.views import ProfileCreateView
 
 app_name = 'profileapp'
 urlpatterns = [
-    path('create/', ProfileCreateView.as_view(),name='create')
+    path('create/', ProfileCreateView.as_view(),name='create'),
+    path('update/<int:pk>', ProfileCreateView.as_view(),name='update')
 ]
