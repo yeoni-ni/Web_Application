96821ae3c1b96848d6db5ac44d1a0ddc75097b2d
kimsy0831 <kimsy0831@liklion.org>
2021-07-27 16:49:19 +0900

    7/27 6class commit1

--- profileapp/views.py
+++ profileapp/views.py
@@ -15,6 +15,12 @@ class ProfileCreateView(CreateView):
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/create.html'
 
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        # 부모 메소드 불러오기
+        return super().form_valid(form)
+
+
 
 
 
