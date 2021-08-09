ef03aacb878c7f1e7893941b6d37d38406f71bf6
kimsy0831 <kimsy0831@liklion.org>
2021-08-09 16:14:59 +0900

    8/9 class 6ban commit 4

--- commentapp/urls.py
+++ commentapp/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from commentapp.views import CommentCreateView
 
 app_name = 'commentapp'
-urlpatterns= [
-    path('create/', CommentCreateView.as_view(), name='create')
-
+urlpatterns = [
+    path('create/', CommentCreateView.as_view(), name='create'),
 ]
