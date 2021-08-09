f64a99df80d2eb5339367ed36ae9c9e447aed674
kimsy0831 <kimsy0831@liklion.org>
2021-08-09 16:22:24 +0900

    8/9 class 6ban commit 5

--- commentapp/urls.py
+++ commentapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from commentapp.views import CommentCreateView
+from commentapp.views import CommentCreateView, CommentDeleteView
 
 app_name = 'commentapp'
 urlpatterns = [
     path('create/', CommentCreateView.as_view(), name='create'),
+    path('delete<ink:pk>', CommentDeleteView.as_view(), name='delete')
 ]
