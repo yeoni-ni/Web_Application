2a4994f4aa1ecea74b850bba8392b7d33d2e14ef
kimsy0831 <kimsy0831@liklion.org>
2021-08-09 14:51:49 +0900

    8/9 class 6ban commit 2

--- commentapp/models.py
+++ commentapp/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+from articleapp.models import Article
+
+
+class Comment(models.Model):
+    # 게시글이 어디와 연결되어있는지
+    article = models.ForeignKey(Article, on_delete=models.SET_NULL,
+                                related_name='comment', null=True)
+    # 작성자 연결
+    writer = models.ForeignKey(User, on_delete=models.SET_NULL,
+                               related_name='comment', null=True)
+    # 내용
+    content = models.TextField(null=False)
+    # 언제 댓글이 작성되었는지
+    created_at = models.DateTimeField(auto_now_add=True)
