96d85ada21f7d0706a6030081351af6b2e3d99b3
kimsy0831 <kimsy0831@liklion.org>
2021-08-19 12:35:56 +0900

    8/19 class 6ban commit 5

--- subscribeapp/models.py
+++ subscribeapp/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+from projectapp.models import Project
+
+
+class Subscription(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE,
+                             related_name='subscription', null=False)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE,
+                                related_name='subscription', null=False)
+    # 1대1로 설정하기 위한 class
+    class Meta:
+        unique_together = ['user', 'project']
